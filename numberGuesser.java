3d94c77cd2ad246cb024d8d5e55cf37847a63be8
Eitvydas Jankauskas <70910743+eitjank@users.noreply.github.com>
2022-04-02 14:51:32 +0300

    user and password login gui

--- numberGuesser.java
+++ numberGuesser.java
@@ -1,28 +1,96 @@
-import java.util.Scanner;
+import javax.swing.*;
+import java.awt.event.*;  
 
-class numberGuesser{
+class numberGuesser implements ActionListener{
+
+    private static JLabel label;
+    private static JTextField userText;
+    private static JLabel label2;
+    private static JPasswordField userText2;
+    private static JButton button;
+    private static JLabel success;
     public static void main(String args[]){
 
-       int number = (int)(Math.random()*100+1);
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,300);
+
+        JPanel panel = new JPanel();
+
+        frame.add(panel);
+
+        panel.setLayout(null);
+
+        label = new JLabel("USER");
+        label.setBounds(10,20,80,25);
+        panel.add(label);
+
+        userText = new JTextField();
+        userText.setBounds(100,20,165,25);
+        panel.add(userText);
+
+        label2 = new JLabel("USER");
+        label2.setBounds(10,50,80,25);
+        panel.add(label2);
+
+        userText2 = new JPasswordField();
+        userText2.setBounds(100,50,165,25);
+        panel.add(userText2);
+        
+        button = new JButton("Press");
+        button.setBounds(10,80,80,25);
+        button.addActionListener(new numberGuesser());
+        panel.add(button);
+
+        success = new JLabel("");
+        success.setBounds(10,110,300,25);
+        panel.add(success);
+        // success.setText("text");
+
+
+        frame.setVisible(true);
+
+
+
+
+    //    JButton button = new JButton("Press");
+    //    frame.getContentPane().add(button); // Adds Button to content pane of frame
+
+        
+
+    //    int number = (int)(Math.random()*100+1);
        
-       System.out.println("Guess a number from 1 to 100");
+    //    System.out.println("Guess a number from 1 to 100");
 
-       Scanner scanner = new Scanner(System.in);
+    //    Scanner scanner = new Scanner(System.in);
 
-       int guess = 0;
-       int numberOfGuesses = 0;
-       while(guess != number){
-            System.out.print("Enter a number: ");
-            guess = scanner.nextInt();
-            numberOfGuesses++;
-            if(guess > number)
-                System.out.println("Too big");
-            else if(guess < number)
-                System.out.println("Too small");
-       }
+    //    int guess = 0;
+    //    int numberOfGuesses = 0;
+    //    while(guess != number){
+    //         System.out.print("Enter a number: ");
+    //         guess = scanner.nextInt();
+    //         numberOfGuesses++;
+    //         if(guess > number)
+    //             System.out.println("Too big");
+    //         else if(guess < number)
+    //             System.out.println("Too small");
+    //    }
+       
+    //    System.out.println("Correct. You guessed it! It is " + number);
+    //    System.out.println("You did it in " + numberOfGuesses + " of guesses.");
 
-       System.out.println("Correct. You guessed it! It is " + number);
-       System.out.println("You did it in " + numberOfGuesses + " of guesses.");
+    }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String user = userText.getText();
+        String password = userText2.getText();
+
+        if(user.equals("Admin") && password.equals("password")){
+            success.setText("Login successful!");
+        }
+        else{
+            success.setText("");
+        }
     }
 }

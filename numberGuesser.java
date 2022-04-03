fd15e6aa028ec39b882a5a3751bcea68f850de35
Eitvydas Jankauskas <70910743+eitjank@users.noreply.github.com>
2022-04-03 12:44:40 +0300

    add game over after 8 tries and change it so doesn't count floating point numbers as valid guesses

--- numberGuesser.java
+++ numberGuesser.java
@@ -10,6 +10,7 @@ class numberGuesser implements ActionListener{
     private static JLabel guessInfo;
     private static JLabel guessCount;
     private static int number;
+    private static int numberOfGuessesStart;
     private static int numberOfGuesses;
     public static void main(String args[]){
 
@@ -41,45 +42,53 @@ class numberGuesser implements ActionListener{
         panel.add(button);
 
         guessInfo = new JLabel("");
-        guessInfo.setBounds(10,110,300,25);
+        guessInfo.setBounds(10,135,300,25);
         panel.add(guessInfo);
 
         guessCount = new JLabel("");
-        guessCount.setBounds(10,135,300,25);
+        guessCount.setBounds(10,110,300,25);
         panel.add(guessCount);
 
         frame.setVisible(true);     
 
         number = (int)(Math.random()*100+1);
 
+        numberOfGuessesStart = 8;
         numberOfGuesses = 0;
+        guessCount.setText("You have " + (numberOfGuessesStart-numberOfGuesses) + " tries left.");
 
     }
 
     public static boolean isNumeric(String str) {
-        return str != null && str.matches("[-+]?\\d*\\.?\\d+");
+        return str != null && str.matches("[-+]?\\d*");
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        String guess = guessText.getText();
-        
-        if(isNumeric(guess)){
-            guessCount.setText("You guessed " + ++numberOfGuesses + " times.");
-            int guessedNumber = Integer.parseInt(guess);
+        if(numberOfGuessesStart-numberOfGuesses>0){
+            String guess = guessText.getText();
+            
+            if(isNumeric(guess)){
+                guessCount.setText("You have " + (numberOfGuessesStart-(++numberOfGuesses)) + " tries left.");
+                int guessedNumber = Integer.parseInt(guess);
 
-            if(guessedNumber==number){
-                guessInfo.setText("Correct. You guessed it! It is " + number + ".");
+                if(guessedNumber==number){
+                    guessInfo.setText("It is " + number + ".");
+                    guessCount.setText("Correct. You have guessed it in " + numberOfGuesses + " tries.");
+                }
+                else{
+                    if(guessedNumber > number)
+                        guessInfo.setText("Too big");
+                    else if(guessedNumber < number)
+                        guessInfo.setText("Too small");
+                }
             }
             else{
-                if(guessedNumber > number)
-                    guessInfo.setText("Too big");
-                else if(guessedNumber < number)
-                    guessInfo.setText("Too small");
+                guessInfo.setText("'" + guess + "' isn't even a number!");
             }
         }
         else{
-            guessInfo.setText("'" + guess + "' isn't even a number!");
+            guessInfo.setText("Game over.");
         }
     }
 }
